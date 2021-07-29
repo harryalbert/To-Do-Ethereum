c75b43c42875c0883846acf9f86d95e731048924
harryalbert <harryalbert364@gmail.com>
2021-07-29 09:55:05 -0400

    added method to add to todo list

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public taskCount = 0;
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("This is the first task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
